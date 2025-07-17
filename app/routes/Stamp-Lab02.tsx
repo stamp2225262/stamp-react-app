8244acb872ece495f79b0a8bba72a0fafca08971
unknown <tamxwyn@gmail.com>
2025-07-17 12:34:10 +0700

    Lab02

---
+++ app/routes/Stamp-Lab02.tsx
@@ -0,0 +1,15 @@
+function MyProflie () {
+ return (
+ <div>
+    <p className="text-2xl font-bold text-gray-800 dark:text-gray-100 flex flex-col items-center justify-center">Stamp Proflie</p>
+    <img  src="/images/Dragonball_Proflie.jpg" className="w-1/3 "></img>
+    <p className="text-lg font-bold text-gray-800 dark:text-gray-100 flex flex-col items-center justify-center">
+        Name: Atthapron Samangyad <br/>
+        Student ID : 026730491003-2 <br/>
+        </p>
+    
+ </div>
+ );
+
+}
+export default MyProflie;
